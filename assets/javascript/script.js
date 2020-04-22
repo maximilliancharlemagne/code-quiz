aab1c0a623fd9b1a42527979db26b0a6e47eea47
Maxwell Roberts <maxwell.k.roberts@gmail.com>
2020-04-22 19:42:58 -0400

    Can progress through questions in order, display end screen

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,27 @@
 // a javascript file
 
-let time = 0
+//Setting up some global variables
+let time = 0 //Time (in seconds) the user has been taking the quiz, for the timer
+let answered = false //Has the user answered this question?
+let index = 0 //What question are we on?
+
+
+//Make a question class
+class question {
+  constructor(title, description, arr_of_answers, num) {
+    this.title = title;
+    this.description = description;
+    this.answers = arr_of_answers;
+    this.indexOfCorrectAnswer = num;
+  }
+}
+
+//Use the question class to make an object for each question
+const question1 = new question('Question 1', 'Which of the below is a valid keyword for declaring a variable in Javascript?', ['var', 'let', 'const', 'All of the above'], 3)
+const question2 = new question('Question 2', 'What will be the result of the following code?\n let num = 5\n let sum = num + 3\n console.log(sum)', ['3 will be logged to the console', '8 will be logged to the console', '2 will be logged to the console', 'The code will result in an error'], 1)
+
+//Make a list of all the questions
+let listOfQuestions = [question1, question2]
 
 function quizWrapper(){
   console.log('Quiz wrapper activated')
@@ -14,10 +35,21 @@ function quizWrapper(){
 }
 
 function buttonHandler(buttonType) {
-  console.log('Start button pressed')
+  console.log(`Button handler called with arg ${buttonType}`)
   switch (buttonType) {
     case 'start':
+      console.log('Start button pressed')
       quizHandler()
+      break;
+    case 'incorrect':
+      console.log('Incorrect answer')
+      index++
+      index == listOfQuestions.length ? endHandler() : questionHandler(listOfQuestions)
+      break;
+    case 'correct':
+      console.log('Correct answer')
+      index++
+      index == listOfQuestions.length ? endHandler() : questionHandler(listOfQuestions)
       break;
 
     default:
@@ -25,26 +57,18 @@ function buttonHandler(buttonType) {
   }
 }
 
+function endHandler(){
+  console.log('I\'m a pretty boy')
+}
+
 function quizHandler() {
   console.log('Quiz handler was called')
-  //Make a question class
-  class question {
-    constructor(title, description, arr_of_answers) {
-      this.title = title;
-      this.description = description;
-      this.answers = arr_of_answers;
-    }
-  }
-
-  //Use the question class to make an object for each question
-  const question1 = new question('Question 1', 'Which of the below is a valid keyword for declaring a variable in Javascript?', ['var', 'let', 'const', 'All of the above'])
-  const question2 = new question('Question 2', 'What will be the result of the following code?\n let num = 5\n let sum = num + 3\n console.log(sum)', ['3 will be logged to the console', '8 will be logged to the console', '2 will be logged to the console', 'The code will result in an error'])
 
   //Start the timer
   let myTimer = window.setInterval(timerUpdater, 1000)
   
-  //Display the first question until it is answered, then display the next question, and so on
-  questionDisplayer(question1,'quiz')
+  //Display the questions
+  questionHandler(listOfQuestions)
 
   //Once all questions have been answered, display the user's score
 }
@@ -55,7 +79,14 @@ function timerUpdater() {
   console.log("Time updated")
 }
 
+function questionHandler(list_of_questions){
+  console.log(`Question handler was called with arg ${list_of_questions}`)
+  questionDisplayer(list_of_questions[index],'quiz')
+
+}
+
 function questionDisplayer(question,elementID){
+  console.log(question)
   //Given a question object, writes the proper CSS
   // to display that question to the .innerHTML property of the
   // HTML element with id = elementID
@@ -72,7 +103,15 @@ function questionDisplayer(question,elementID){
     css+=`<div class = "text-left">`
     css+=`  <ol>\n`
     for (let index in question.answers){
-      css+=`    <li>${question.answers[index]}</li>\n`
+      if (index!=question.indexOfCorrectAnswer){
+      // css+=`    <li>${question.answers[index]}</li>\n`
+      css += `<button type="button" id = "incorrectAnswer" onclick = "buttonHandler('incorrect')" class="btn btn-danger m-1">${question.answers[index]}</button>\n`
+      css += `<br>`
+      }
+      else{
+        css += `<button type="button" id = "correctAnswer" onclick = "buttonHandler('correct')" class="btn btn-danger m-1">${question.answers[index]}</button>\n`
+        css += `<br>`
+      }
     }
     css+=`  </ol>`
     css+=`</div>`
