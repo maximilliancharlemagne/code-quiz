81314c7bba3d167b2f5676e64ff74694b21bcbd5
Maxwell Roberts <maxwell.k.roberts@gmail.com>
2020-04-22 16:52:52 -0400

    Reordered functions in the order they get used

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,6 +12,45 @@ function quizWrapper(){
   document.getElementById("startBtn").addEventListener("click", buttonHandler('start'));
 }
 
+function buttonHandler(buttonType) {
+  switch (buttonType) {
+    case 'start':
+      quizHandler()
+      break;
+
+    default:
+      break;
+  }
+}
+
+function quizHandler() {
+  //Make a question class
+  class question {
+    constructor(title, description, arr_of_answers) {
+      this.title = title;
+      this.description = description;
+      this.answers = arr_of_answers;
+    }
+  }
+
+  //Use the question class to make an object for each question
+  const question1 = new question('Question 1', 'Which of the below is a valid keyword for declaring a variable in Javascript?', ['var', 'let', 'const', 'All of the above'])
+  const question2 = new question('Question 2', 'What will be the result of the following code?\n let num = 5\n let sum = num + 3\n console.log(sum)', ['3 will be logged to the console', '8 will be logged to the console', '2 will be logged to the console', 'The code will result in an error'])
+
+  //Start the timer
+  timer(1000)
+  //Display the first question until it is answered, then display the next question, and so on
+
+  //Once all questions have been answered, display the user's score
+}
+
+function timer(interval) {
+  setInterval(timerUpdater, interval)
+}
+
+function timerUpdater() {
+
+}
 
 function questionDisplayer(question,elementID){
   //Given a question object, writes the proper CSS
@@ -35,36 +74,4 @@ function questionDisplayer(question,elementID){
       li question.answers[index]
     /ol
   */
-}
-
-function buttonHandler(buttonType){
-  switch (buttonType) {
-    case 'start':
-      quizHandler()
-      break;
-  
-    default:
-      break;
-  }
-}
-
-function quizHandler(){
-  //Make a question class
-  class question {
-    constructor(title, description, arr_of_answers) {
-      this.title = title;
-      this.description = description;
-      this.answers = arr_of_answers;
-    }
-  }
-
-  //Use the question class to make an object for each question
-  const question1 = new question('Question 1', 'Which of the below is a valid keyword for declaring a variable in Javascript?', ['var', 'let', 'const', 'All of the above'])
-  const question2 = new question('Question 2', 'What will be the result of the following code?\n let num = 5\n let sum = num + 3\n console.log(sum)', ['3 will be logged to the console', '8 will be logged to the console', '2 will be logged to the console', 'The code will result in an error'])
-
-  //Start the timer
-
-  //Display the first question until it is answered, then display the next question, and so on
-
-  //Once all questions have been answered, display the user's score
 }
